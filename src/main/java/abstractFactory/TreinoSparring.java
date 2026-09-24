07c4f21fcf0a31caef6b8d13d6112e96c9d98bd6
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2026-09-23 22:00:45 -0300

    Cria dois tipos de treino: de sparring e de técnica

---
+++ src/main/java/abstractFactory/TreinoSparring.java
@@ -0,0 +1,11 @@
+package abstractFactory;
+
+public class TreinoSparring implements Treino{
+
+    public String iniciar(){
+        return "Treinar: 2min de luta completa";
+    }
+
+}
+
+
---
+++ src/main/java/abstractFactory/TreinoTecnico.java
@@ -0,0 +1,9 @@
+package abstractFactory;
+
+public class TreinoTecnico implements Treino{
+
+    public String iniciar(){
+        return "Treinar: boxe sombra, rounds saco de pancada, sequências na manopla";
+    }
+
+}
