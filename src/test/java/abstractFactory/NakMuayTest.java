37b2d8af3a34ac83f1449d1062b3ec9308dcc3f6
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2026-09-23 22:27:55 -0300

    Upload diagrama de classes

--- src/test/java/abstractFactory/NakMuayTest.java
+++ src/test/java/abstractFactory/NakMuayTest.java
@@ -3,7 +3,7 @@ package abstractFactory;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class NakMuayTest {
+class NakMuayTest    {
 
     @Test
     void deveIniciarTreinoTecnico(){
