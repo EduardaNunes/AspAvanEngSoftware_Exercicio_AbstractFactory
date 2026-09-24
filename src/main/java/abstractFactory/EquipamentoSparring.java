7122e4e1836bebbaaf41176434a3a739edae6037
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2026-09-23 21:58:11 -0300

    Cria dois tipos de equipamento: para sparring e para técnica

---
+++ src/main/java/abstractFactory/EquipamentoSparring.java
@@ -0,0 +1,7 @@
+package abstractFactory;
+
+public class EquipamentoSparring implements Equipamento{
+    public String colocar(){
+        return "Equipado: Bandagem de 5 metros, luva 16oz, caneleira e protetor bocal";
+    }
+}
---
+++ src/main/java/abstractFactory/EquipamentoTecnico.java
@@ -0,0 +1,7 @@
+package abstractFactory;
+
+public class EquipamentoTecnico implements Equipamento{
+    public String colocar(){
+        return "Equipado: Bandagem de 3 metros, luva 12oz e caneleira";
+    }
+}
